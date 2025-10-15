4793fe7bcc32e9aed88048341e339da5ee6fb05f
raulsr92 <raul.sr92@gmail.com>
2025-10-15 17:34:49 -0400

    Initial Commit

---
+++ AppElecciones26/AppElecciones26App.swift
@@ -0,0 +1,17 @@
+//
+//  AppElecciones26App.swift
+//  AppElecciones26
+//
+//  Created by user278624 on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AppElecciones26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
